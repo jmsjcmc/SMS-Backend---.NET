9aa9e2597adab8538f0d32c57be57b8ef0bf38df
jmsjcmc <jecemecojamestabilog.com>
2025-12-22 17:12:58 +0800

    feat: add create role validator

---
+++ Migrations/20251222051756_5.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SMS_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class _5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Classroom",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AdviserID = table.Column<int>(type: "int", nullable: true),
+                    SchoolYear = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classroom", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Classroom_Users_AdviserID",
+                        column: x => x.AdviserID,
+                        principalTable: "Users",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subject",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Day = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Hours = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TeacherID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subject", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Subject_Users_TeacherID",
+                        column: x => x.TeacherID,
+                        principalTable: "Users",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClassroomLog",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClassroomID = table.Column<int>(type: "int", nullable: true),
+                    UpdaterID = table.Column<int>(type: "int", nullable: true),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClassroomLog", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_ClassroomLog_Classroom_ClassroomID",
+                        column: x => x.ClassroomID,
+                        principalTable: "Classroom",
+                        principalColumn: "ID");
+                    table.ForeignKey(
+                        name: "FK_ClassroomLog_Users_UpdaterID",
+                        column: x => x.UpdaterID,
+                        principalTable: "Users",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubjectLog",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SubjectID = table.Column<int>(type: "int", nullable: true),
+                    UpdaterID = table.Column<int>(type: "int", nullable: true),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubjectLog", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_SubjectLog_Subject_SubjectID",
+                        column: x => x.SubjectID,
+                        principalTable: "Subject",
+                        principalColumn: "ID");
+                    table.ForeignKey(
+                        name: "FK_SubjectLog_Users_UpdaterID",
+                        column: x => x.UpdaterID,
+                        principalTable: "Users",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Classroom_AdviserID",
+                table: "Classroom",
+                column: "AdviserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClassroomLog_ClassroomID",
+                table: "ClassroomLog",
+                column: "ClassroomID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClassroomLog_UpdaterID",
+                table: "ClassroomLog",
+                column: "UpdaterID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subject_TeacherID",
+                table: "Subject",
+                column: "TeacherID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubjectLog_SubjectID",
+                table: "SubjectLog",
+                column: "SubjectID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubjectLog_UpdaterID",
+                table: "SubjectLog",
+                column: "UpdaterID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClassroomLog");
+
+            migrationBuilder.DropTable(
+                name: "SubjectLog");
+
+            migrationBuilder.DropTable(
+                name: "Classroom");
+
+            migrationBuilder.DropTable(
+                name: "Subject");
+        }
+    }
+}
---
+++ Models/Validators/RoleValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace SMS_backend.Models
+{
+    public class CreateRoleValidator : AbstractValidator<CreateRoleRequest>
+    {
+        public CreateRoleValidator()
+        {
+            RuleFor(X => X.Name)
+                .NotEmpty().WithMessage("NAME REQUIRED");
+        }
+    }
+}
